61439137c035f94e411a83573394ee28a5b7ca05
fresh-tml <stephenchijoke1@gmail.com>
2022-09-12 03:44:13 -0700

    var

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Determines if a number is positive, negative or zero.
  * Description: using the main function
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
