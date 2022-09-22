9fb70f9d5b076a6f4efdada71151d231e9084ca9
fresh-tml <stephenchijoke1@gmail.com>
2022-09-22 13:57:47 -0700

    pointer

--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,10 @@
  here is more on pointers
+0. write a function that concatenates two strings
+1. same with 0.
+2. write a function that copies a string
+3. write a function that compares two strings.
+4. write a function that reverses that content of an array of integers.
+5. write a function that changes the lowercase letter of a string to uppercase.
+6. write a function that capitalize all words of a string.
+7. write a function that encodes a string into 1337.
+8. write a function that encodes a string using rot13.
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 /**
  * File: main.h
@@ -7,10 +8,10 @@
  * Description: Header file containing prototypes for all functions
  */
 
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
@@ -21,4 +22,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
-#endif
+#endif /*MAIN_H*/
