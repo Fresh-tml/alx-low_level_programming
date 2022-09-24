368b7050593a639b903aa12b5340fd4fe3943906
fresh-tml <stephenchijoke1@gmail.com>
2022-09-23 21:25:53 -0700

    fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * _strncpy - copies at most an inputted number of bytes from string src
- *  @dest: the buffer storing the string copy.
- *  @src: the source string
- *  @n: the maximum number of bytes copied from src
- *  Return: a pointer to the resulting string dest
+ * _strncpy - for copyying
+ *  @dest: parameter 1
+ *  @src: parameter 2
+ *  @n: parameter 3
+ *  Return: string
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)\
 {
 	strncpy(dest, src, n);
 	return (dest);
