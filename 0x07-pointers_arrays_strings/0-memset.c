2fe05d6a3f8608447621333470cf5549d2502f8b
fresh-tml <stephenchijoke1@gmail.com>
2022-09-27 14:13:49 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fill a memory block
+ * @s: starting address
+ * @b: value
+ * @n: no of bytes
+ * Return: always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i = 0;
+
+	for (n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
